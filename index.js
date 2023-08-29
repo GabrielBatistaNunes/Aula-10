d1e36b6370671797f7b79869ac99074f7ee908c7
GabrielBatistaNunes <142436040+GabrielBatistaNunes@users.noreply.github.com>
2023-08-29 16:25:25 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+// 1.
+// let array;
+// //criou uma variavel
+// console.log('a. ', array);
+// //a undefined
+// array = null;
+// console.log('b. ', array);
+// //b null
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// console.log('c. ', array.length);
+// //c 11
+// let i = 0;
+// console.log('d. ', array[i]);
+// array[i+1] = 19;
+// //d 3
+// console.log('e. ', array);
+// //e (11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// const valor = array[i+6];
+// console.log('f. ', valor);
+// //f 9
+
+// 2.
+/*
+const frase = prompt("Digite uma frase");
+//"Subi num ônibus em Marrocos"
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length);
+//SUBI NUM ÔNIBUS EM MIRROCOS 27
+*/
+
+// B 1.
+// let nome = prompt("seu nome");
+// let email = prompt("escreva seu e-mail");
+// console.log("O e-mail " + email + " foi cadastrado com sucesso. Seja bem-vinda(o), " + nome + "!");
+
+// B 2.
+// let comidas = ["pavê", "hamburguer", "brócolis", "sushi", "mousse"];
+// console.log(comidas);
+// console.log("essas são minhas comidas preferidas");
+// console.log(comidas[0]);
+// console.log(comidas[1]);
+// console.log(comidas[2]);
+// console.log(comidas[3]);
+// console.log(comidas[4]);
+// // nova lista
+// console.log([comidas[0], comidas[1] = prompt("escolha um alimento para substituir hamburguer"), comidas[2], comidas[3], comidas[4]]);
+
+// B 3.
+// let listaDeTarefa = [];
+// listaDeTarefa = [prompt("1 tarefa"), prompt("2 tarefa"), prompt("3 tarefa")];
+// console.log(listaDeTarefa);
+// n = prompt("escolha de 0 a 2 as 3 tarefas que vc escolheu");
+// console.log(listaDeTarefa[n]);
+// listaDeTarefa.splice(n, 1);
+// console.log(listaDeTarefa);
+
+// D1.
+// frase = prompt("escreva uma frase");
+// let f = frase.split(" ");
+// console.log(f);
+
+//D2.
+// frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+// console.log("ID " + frutas.indexOf("Abacaxi") + ". nº de frutas " + frutas.length);
